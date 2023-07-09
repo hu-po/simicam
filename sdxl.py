c4839042328b0257b2e861ec84201ae1b8962775
hu.po <hugo.p.cmu@gmail.com>
2023-07-09 17:31:38 -0500

    sdxl almost there

--- sdxl.py
+++ sdxl.py
@@ -1,59 +1,75 @@
+import argparse
 import asyncio
-import json
 import logging
-from pprint import pformat
+from typing import Any
 
-import torch
-import zmq
-import zmq.asyncio
-from diffusers import DiffusionPipeline, StableDiffusionPipeline
-from diffusers.models import AutoencoderKL
+import numpy as np
 from omegaconf import OmegaConf
-from sgm.util import load_model_from_config
+from PIL import Image
+import sgm
+from sgm.models.autoencoder import AutoencoderKLInferenceWrapper
+from sgm.util import instantiate_from_config, load_safetensors
+
+from utils import get_device, microservice_server, time_and_log
 
 log = logging.getLogger(__name__)
-
-
-async def receive_request_async(ip: str = "127.0.0.1", port: str = "5555") -> None:
-    log.info(f"Listening on {ip}:{port} ...")
-    context = zmq.asyncio.Context()
-    socket = context.socket(zmq.REP)
-    socket.bind(f"tcp://{ip}:{port}")
-    while True:
-        log.info("Waiting for a request...")
-        message = await socket.recv_json()
-        request = json.loads(message)
-        log.info(f"Received request: {pformat(request)}")
-
-        if "message" in request:
-            response = {"response": request["message"][::-1]}
-        else:
-            response = {"error": 'No "message" field in request'}
-
-        log.info(f"Sending response: {pformat(response)}")
-        await socket.send_json(json.dumps(response))
+args = argparse.ArgumentParser()
+args.add_argument("--test", action="store_true")
 
 def load_vae(
-    config='/workspace/generative-models/checkpoints/sdxl_vae.json',
-    ckpt='/workspace/generative-models/checkpoints/sdxl_vae.safetensors',
+    config: str = '/workspace/generative-models/checkpoints/sdxl_vae.yaml',
+    ckpt: str = '/workspace/generative-models/checkpoints/sdxl_vae.safetensors',
+    verbose: bool = False,
+    device: str = "gpu",
 ):
+    _ = get_device("gpu")
     config = OmegaConf.load(config)
-    model, msg = load_model_from_config(config, ckpt)
+    model = instantiate_from_config(config.model)
+    sd = load_safetensors(ckpt)
+    m, u = model.load_state_dict(sd, strict=False)
+    if len(m) > 0 and verbose:
+        print("missing keys:")
+        print(m)
+    if len(u) > 0 and verbose:
+        print("unexpected keys:")
+        print(u)
+    model.cuda()
+    model.eval()
     return model
 
-def model_inference(
-    device="cuda" if torch.cuda.is_available() else "cpu",
+@time_and_log
+def vae_encode(
+    image: np.ndarray = None,
+    model: Any = None,
+    **kwargs
 ):
-    # model_weights_path = ''
-    # model = "stabilityai/your-stable-diffusion-model"
-    
-    # This downloads the model into ~/.cache/huggingface/hub
-    vae = AutoencoderKL.from_pretrained("stabilityai/sdxl-vae")
-    
-    # vae = AutoencoderKL.from_pretrained(f"{model_weights_path}/sdxl_vae.safetensors")
-    print(f"vae: {vae}")
-    # pipe = StableDiffusionPipeline.from_pretrained(model, vae=vae)
+    log.debug("Encoding with VAE")
+    latent = model.encode(image)
+    log.debug(f"Latent {latent}")
+    return latent
 
+def test_vae(
+    image_filepath="data/test.png",
+):
+    log.debug("Testing Loading VAE")
+    model = load_vae(
+        config='./ckpt/sdxl_vae.yaml',
+        ckpt='./ckpt/sdxl_vae.safetensors',
+    )
+    image = np.array(Image.open(image_filepath))
+    log.debug(f"Image shape {image.shape}")
+    log.debug("Testing Encode")
+    latent = vae_encode(model=model, image=image)
+    log.debug(f"Latent shape {latent.shape}")
+
+def load_vae_diffusers(
+    model_weights_path='/workspace/generative-models/checkpoints',
+):
+    # SEEMS BROKEN, USE LOCAL GIT REPO INSTEAD
+    from diffusers import DiffusionPipeline, StableDiffusionPipeline
+    from diffusers.models import AutoencoderKL
+    vae = AutoencoderKL.from_pretrained("stabilityai/sdxl-vae")
+    # pipe = StableDiffusionPipeline.from_pretrained(model, vae=vae)
     # pipe = DiffusionPipeline.from_pretrained(
     #     # "stabilityai/stable-diffusion-xl-base-0.9",
     #     f"{model_weights_path}/sd_xl_base_0.9.safetensors",
@@ -62,19 +78,24 @@ def model_inference(
     #     variant="fp16",    
     #     )
     # pipe.to(device)
-
     # # if using torch < 2.0
     # pipe.enable_xformers_memory_efficient_attention()
-
     # prompt = "An astronaut riding a green horse"
     # pipe(prompt=prompt).images[0]
+    return vae
+
+
 
 if __name__ == "__main__":
-    BIND_URI = "tcp://*:5555"
+    args = args.parse_args()
+    if args.test:
+        log.info("Testing SDXL")
+        test_vae()
+    else:
+        log.info("Starting SDXL microservice")
+        asyncio.run(
+            microservice_server(init_func=load_vae, loop_func=process_request)
+        )
 
-    model = load_vae()
-    # model = model_inference()
-    
-    # Server Side
-    # asyncio.run(receive_request_async())
+
 
