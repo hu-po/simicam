0790307882d152ded1d84e0b4ec18c3e8dab99bb
hu.po <hugo.p.cmu@gmail.com>
2023-07-09 13:49:54 -0500

    working micro test

--- test_microservice.py
+++ test_microservice.py
@@ -4,34 +4,27 @@ import logging
 
 from utils import microservice_client, microservice_server
 
-log = logging.getLogger(__name__)
-
 parser = argparse.ArgumentParser()
 parser.add_argument('--server', action='store_true')
 parser.add_argument('--client', action='store_true')
 parser.add_argument('--client_sam', action='store_true')
 parser.add_argument('--client_sdxl', action='store_true')
 
-
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
     args = parser.parse_args()
-
     if args.server:
         def mock_load_model():
             return {
                 "model": lambda x: x+1,
-                "device": "device",
             }
-        def mock_inference(x, model=None):
-            return model(x)
+        def mock_inference(model=None, request=None):
+            return model(request["x"])
         asyncio.run(microservice_server(init_func=mock_load_model, loop_func=mock_inference))
-
     if args.client:
         def mock_request():
-            return 1
+            return {"x": 1}
         asyncio.run(microservice_client(request_func=mock_request))
-
-
     if args.client_sam:
         def mock_camera():
             return {
