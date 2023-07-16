4b043b0ce398b7998bfe932b66062527f6db9628
hu.po <hugo.p.cmu@gmail.com>
2023-07-16 12:19:14 -0500

    debug rpi dockerfile

---
+++ launch.cam.rpi.sh
@@ -0,0 +1,14 @@
+export DATA_PATH="/home/pi/simicam/data"
+export CKPT_PATH="/home/pi/simicam/ckpt"
+export LOGS_PATH="/home/pi/simicam/logs"
+docker build \
+     -t "simicam/rpi" \
+     -f Dockerfile.rpi .
+docker run \
+    -t \
+    --rm \
+    -p 5555:5555 \
+    -v ${DATA_PATH}:/workspace/data \
+    -v ${CKPT_PATH}:/workspace/ckpt \
+    -v ${LOGS_PATH}:/workspace/logs \
+    simicam/rpi
