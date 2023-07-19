811277cb6f8e3b35b25aec5a19fad77d7fe44ae0
hu.po <hugo.p.cmu@gmail.com>
2023-07-19 15:01:44 -0500

    cuda too old

--- launch.llama.sh
+++ launch.llama.sh
@@ -1,12 +1,21 @@
+export DATA_PATH="/home/pi/simicam/data"
+export CKPT_PATH="/home/pi/simicam/ckpt"
+export LOGS_PATH="/home/pi/simicam/logs"
 docker build \
-     -t "llama/13b" \
+     -t "simicam/llama-13b" \
      -f Dockerfile.llama .
 docker run \
     --gpus all \
     -it \
     --rm \
-    -v /home/oop/dev/llama/llama-2-13b:/llama/llama-2-13b \
-    -v /home/oop/dev/llama/llama-2-13b-chat:/llama/llama-2-13b-chat \
-    -v /home/oop/dev/llama/tokenizer_checklish.chk:/llama/tokenizer_checklish.chk \
-    -v /home/oop/dev/llama/tokenizer.model:/llama/tokenizer.model \
-    llama/13b
+    -v ${DATA_PATH}:/workspace/data \
+    -v ${LOGS_PATH}:/workspace/logs \
+    -v ${CKPT_PATH}/llama-2-13b:/llama/llama-2-13b \
+    -v ${CKPT_PATH}/llama-2-13b-chat:/llama/llama-2-13b-chat \
+    -v ${CKPT_PATH}/llama-tokenizer/tokenizer_checklish.chk:/llama/tokenizer_checklish.chk \
+    -v ${CKPT_PATH}/llama-tokenizer/tokenizer.model:/llama/tokenizer.model \
+    simicam/llama-13b \
+    torchrun --nproc_per_node 1 example_chat_completion.py \
+    --ckpt_dir llama-2-13b-chat/ \
+    --tokenizer_path tokenizer.model \
+    --max_seq_len 512 --max_batch_size 4
