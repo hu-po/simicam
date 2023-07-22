fccfe990bccade1fe35b70da1e020e7b8bffb3e6
hu.po <hugo.p.cmu@gmail.com>
2023-07-22 06:32:53 -0500

    fun with dask

--- launch.dask.scheduler.sh
+++ launch.dask.scheduler.sh
@@ -1,10 +1,13 @@
 # https://docs.dask.org/en/latest/deploying-docker.html
 # docker network create dask
     # --network dask \
-docker run \
-    --rm \
-    -p 8786:8786 \
-    -p 8787:8787 \
-    --name scheduler \
-    ghcr.io/dask/dask \
-    dask-scheduler
+# docker run \
+#     --rm \
+#     -p 8786:8786 \
+#     -p 8787:8787 \
+#     --name scheduler \
+#     ghcr.io/dask/dask \
+#     dask-scheduler
+dask scheduler \
+    --port 8786 \
+    --dashboard-address 8787
--- run.oop.py
+++ run.oop.py
@@ -1,8 +1,39 @@
 import asyncio
-from dask.distributed import Scheduler, Worker
+import cv2 
+import zlib
+from datetime import datetime
+from dask.distributed import Worker 
 
-async def f(scheduler_address):
-    w = await Worker(scheduler_address)
-    await w.finished()
+async def camera_worker(scheduler_address):
 
-asyncio.get_event_loop().run_until_complete(f("tcp://172.18.0.2:8786"))
+  async with Worker(scheduler_address) as worker:
+
+    camera = cv2.VideoCapture(0)
+    camera.set(cv2.CAP_PROP_FRAME_WIDTH, 256)
+    camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 256) 
+    camera.set(cv2.CAP_PROP_FPS, 30)
+
+    while True:
+
+      ret, image = camera.read()
+      if not ret:
+        print("Failed to capture frame")
+        continue
+
+      image_bytes = cv2.imencode('.jpg', image)[1].tobytes()
+      compressed_image = zlib.compress(image_bytes)
+
+      image_data = {
+        'image': compressed_image,
+        'image_timestamp': datetime.now()  
+      }
+
+      await worker.loop.run_in_executor(None, 
+        worker.update_data, image_data)
+
+      await asyncio.sleep(0.033) 
+
+if __name__ == '__main__':
+
+  asyncio.get_event_loop().run_until_complete(
+    camera_worker('tcp://192.168.1.35:8786'))
