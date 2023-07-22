6357cc99c0ac3aea42c0185072b571b2d3c687f1
hu.po <hugo.p.cmu@gmail.com>
2023-07-22 08:40:05 -0500

    dask is kinda sus

--- run.oop.py
+++ run.oop.py
@@ -1,39 +1,41 @@
 import asyncio
-import cv2 
+import cv2
 import zlib
 from datetime import datetime
-from dask.distributed import Worker 
+from dask.distributed import Client
 
 async def camera_worker(scheduler_address):
+    with Client(scheduler_address) as client:
 
-  async with Worker(scheduler_address) as worker:
+        camera = cv2.VideoCapture(0)
+        camera.set(cv2.CAP_PROP_FRAME_WIDTH, 256)
+        camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 256)
+        camera.set(cv2.CAP_PROP_FPS, 30)
 
-    camera = cv2.VideoCapture(0)
-    camera.set(cv2.CAP_PROP_FRAME_WIDTH, 256)
-    camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 256) 
-    camera.set(cv2.CAP_PROP_FPS, 30)
+        while True:
 
-    while True:
+            ret, image = camera.read()
+            if not ret:
+                print("Failed to capture frame")
+                continue
 
-      ret, image = camera.read()
-      if not ret:
-        print("Failed to capture frame")
-        continue
+            image_bytes = cv2.imencode('.jpg', image)[1].tobytes()
+            compressed_image = zlib.compress(image_bytes)
 
-      image_bytes = cv2.imencode('.jpg', image)[1].tobytes()
-      compressed_image = zlib.compress(image_bytes)
+            image_data = {
+                'image': compressed_image,
+                'image_timestamp': datetime.now()
+            }
 
-      image_data = {
-        'image': compressed_image,
-        'image_timestamp': datetime.now()  
-      }
-
-      await worker.loop.run_in_executor(None, 
-        worker.update_data, image_data)
-
-      await asyncio.sleep(0.033) 
+            await client.scatter(image_data,
+                    workers=[
+                        'tcp://192.168.1.15:38325', #rpi
+                        # TODO: tren
+                    ],
+                    direct=True,
+                    broadcast=True,
+                    asynchronous=True,
+            )
 
 if __name__ == '__main__':
-
-  asyncio.get_event_loop().run_until_complete(
-    camera_worker('tcp://192.168.1.35:8786'))
+    asyncio.run(camera_worker('tcp://192.168.1.35:8786'))
--- run.pi.py
+++ run.pi.py
@@ -1,8 +1,13 @@
 import asyncio
-from dask.distributed import Scheduler, Worker
+from dask.distributed import Worker
 
-async def f(scheduler_address):
+async def worker(scheduler_address):
+    print("Worker here 1", scheduler_address)
     w = await Worker(scheduler_address)
+    for _ in range(10):
+        print("Worker here 2", w.address)
+        image_data = await w.gather('image_data')
+        print(image_data)
     await w.finished()
 
-asyncio.get_event_loop().run_until_complete(f("tcp://192.168.1.35:8786"))
+asyncio.get_event_loop().run_until_complete(worker("tcp://192.168.1.35:8786"))
