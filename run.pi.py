7d315f0aabdb00c77d8c9f8dfc91b2f74abe8f03
hu.po <hugo.p.cmu@gmail.com>
2023-07-22 05:28:13 -0500

    working Pi communication

--- run.pi.py
+++ run.pi.py
@@ -5,4 +5,4 @@ async def f(scheduler_address):
     w = await Worker(scheduler_address)
     await w.finished()
 
-asyncio.get_event_loop().run_until_complete(f("tcp://172.18.0.2:8786"))
+asyncio.get_event_loop().run_until_complete(f("tcp://192.168.1.35:8786"))
