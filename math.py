075fb5707b474edc0b3c1328ff4cb2ce7a1150a3
hu.po <hugo.p.cmu@gmail.com>
2023-08-05 08:53:38 -0500

    wutology

--- math.py
+++ math.py
@@ -1,9 +1,14 @@
 import math
 
 almost_pi: float = 22/7
-almost_e: float = 19/7
 error_pi: float = math.pi - almost_pi
+
+almost_e: float = 19/7
 error_e: float = math.e - almost_e
+
+e_to_i_pi: float = math.e ** (math.pi * 1j)
+almost_e_to_i_pi: float = almost_e ** (almost_pi * 1j)
+error_e_to_i_pi: float = e_to_i_pi - almost_e_to_i_pi
 
 print(f"pi is {math.pi}")
 print(f"almost_pi is {almost_pi}")
@@ -11,3 +16,5 @@ print(f"error_pi is {error_pi}")
 print(f"e is {math.e}")
 print(f"almost_e is {almost_e}")
 print(f"error_e is {error_e}")
+print(f"e_to_i_pi is {e_to_i_pi}")
+print(f"almost_e_to_i_pi is {almost_e_to_i_pi}")
