5cad6a3a9e1052890817d0d48ed67dd859deec25
hu.po <hugo.p.cmu@gmail.com>
2023-08-08 06:23:28 -0500

    starter quantum tutorial

--- quantum.py
+++ quantum.py
@@ -1,20 +1,26 @@
-'''
+"""
+conda create -n quantum python=3.10
+conda activate quantum
+pip install pennylane --upgrade
+pip install pennylane-lightning pennylane-lightning[gpu]
+
 https://github.com/PennyLaneAI/pennylane
+https://pennylane.ai/qml/demos/tutorial_qubit_rotation
 https://pennylane.ai/qml/demos/tutorial_qft_arithmetics
-'''
+"""
 
 import pennylane as qml
 import matplotlib.pyplot as plt
+from pennylane import numpy as np
 
-dev = qml.device("default.qubit", wires=3)
+dev1 = qml.device("lightning.qubit", wires=1)
 
-@qml.qnode(dev)
-@qml.compile()
-def basis_embedding_circuit(m):
-    qml.BasisEmbedding(m, wires=range(3))
-    return qml.state()
 
-m = 6 # number to be encoded
+@qml.qnode(dev1, interface="autograd")
+def circuit(phi1, phi2):
+    qml.RX(phi1, wires=0)
+    qml.RY(phi2, wires=0)
+    return qml.expval(qml.PauliZ(0))
 
-qml.draw_mpl(basis_embedding_circuit, show_all_wires=True)(m)
-plt.show()
+
+print(circuit(0.54, 0.12))
