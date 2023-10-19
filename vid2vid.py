df819d6764b8525c6a0991a49ff2bbff67c633bc
hu.po <hugo.p.cmu@gmail.com>
2023-10-19 07:23:37 -0500

    almost working

--- vid2vid.py
+++ vid2vid.py
@@ -4,35 +4,40 @@ conda activate vid2vid
 pip install moviepy requests replicate
 
 # https://replicate.com/jagilley/controlnet-pose
-docker run -p 5000:5000 --gpus=all r8.im/jagilley/controlnet-pose@sha256:0304f7f774ba7341ef754231f794b1ba3d129e3c46af3022241325ae0c50fb99
+docker run -p 5000:5000 \
+    --name controlnet_container \
+    --gpus=all r8.im/jagilley/controlnet-pose@sha256:0304f7f774ba7341ef754231f794b1ba3d129e3c46af3022241325ae0c50fb99
+docker commit controlnet_container controlnet_container
+
+
 
 # https://replicate.com/cjwbw/rembg
-docker run -d -p 5000:5000 --gpus=all r8.im/cjwbw/rembg@sha256:fb8af171cfa1616ddcf1242c093f9c46bcada5ad4cf6f2fbe8b81b330ec5c003
+docker run -d -p 5000:5000 \
+    --name bgremoval_container \
+    --gpus=all r8.im/cjwbw/rembg@sha256:fb8af171cfa1616ddcf1242c093f9c46bcada5ad4cf6f2fbe8b81b330ec5c003
+docker commit bgremoval_container bgremoval_container
 """
 
+import base64
+import glob
 import os
-import requests
+import subprocess
+import urllib.request
 import uuid
+from io import BytesIO
+
+import requests
+from PIL import Image
+
 
 def process_video_frames(
     input_video_path="/home/oop/dev/simicam/data/test.mp4",
     docker_url="http://localhost:5000/predictions",
     base_output_dir="/home/oop/dev/simicam/logs",
-    params_controlnet = {
-            "scale": 9,
-            "prompt": "an astronaut on the moon, digital art",
-            "a_prompt": "best quality, extremely detailed",
-            "n_prompt": "longbody, lowres, bad anatomy, bad hands, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality",
-            "ddim_steps": 20,
-            "num_samples": "1",
-            "image_resolution": "512",
-            "detect_resolution": 512
-    },
-    params_bgremoval = {},
 ):
     # Generate a unique id for this generation session
     session_id = uuid.uuid4()
-    
+
     # Create a output folder for the session id and use that as the output dir
     output_dir = os.path.join(base_output_dir, str(session_id))
     os.makedirs(output_dir, exist_ok=True)
@@ -41,30 +46,75 @@ def process_video_frames(
     os.system(f"ffmpeg -i {input_video_path} -vf fps=1 {output_dir}/raw_%05d.png")
 
     # Run the controlnet docker container
-    os.system("docker run -p 5000:5000 --gpus=all r8.im/jagilley/controlnet-pose@sha256:0304f7f774ba7341ef754231f794b1ba3d129e3c46af3022241325ae0c50fb99")
-    
-    # Feed each frame to the controlnet docker container
-    for i, frame in enumerate(clip.iter_frames()):
-        frame_path = os.path.join(output_dir, f"raw_{i:05}.png")
-        params["input"]["image"] = open(frame_path, "rb")
-        response = requests.post(docker_url, headers={"Content-Type": "application/json"}, json=params)
-        print(response.json())
+    docker_process = subprocess.Popen(
+        "docker run -d -p 5000:5000 --gpus=all controlnet_container"
+    )
+
+    # Feed each frame to the controlnet docker container and save the output image
+    for i, frame_path in enumerate(glob.glob(os.path.join(output_dir, "raw_*.png"))):
+        with open(frame_path, "rb") as img_file:
+            response = requests.post(
+                docker_url,
+                headers={"Content-Type": "application/json"},
+                json={
+                    "input": {
+                        "image": f"data:image/png;base64,{base64.b64encode(img_file.read()).decode('utf-8')}",
+                        "prompt": "an astronaut on the moon, digital art",
+                        "a_prompt": "best quality, extremely detailed",
+                        "n_prompt": "longbody, lowres, bad anatomy, bad hands, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality",
+                        "ddim_steps": 20,
+                        "num_samples": "1",
+                        "image_resolution": "512",
+                        "detect_resolution": 512,
+                    },
+                },
+            )
+        img = Image.open(
+            BytesIO(base64.b64decode(response.json()["output"][0].split(",")[1]))
+        )
+        img.save(os.path.join(output_dir, f"controlnet_pose_{i:05}.png"))
+        img = Image.open(
+            BytesIO(base64.b64decode(response.json()["output"][1].split(",")[1]))
+        )
+        img.save(os.path.join(output_dir, f"controlnet_full_{i:05}.png"))
 
     # Kill controlnet docker container
+    docker_process.terminate()
     os.system("docker kill $(docker ps -q)")
 
     # Run the bgremoval docker container
-    os.system("docker run -d -p 5000:5000 --gpus=all r8.im/cjwbw/rembg@sha256:fb8af171cfa1616ddcf1242c093f9c46bcada5ad4cf6f2fbe8b81b330ec5c003")
+    docker_process = subprocess.Popen(
+        "docker run -d -p 5000:5000 --gpus=all bgremoval_container"
+    )
 
     # Feed each frame to the bgremoval docker container
-    for i, frame in enumerate(clip.iter_frames()):
-        frame_path = os.path.join(output_dir, f"raw_{i:05}.png")
-        params["input"]["image"] = open(frame_path, "rb")
-        response = requests.post(docker_url, headers={"Content-Type": "application/json"}, json=params)
-        print(response.json())
+    for i, frame_path in enumerate(
+        glob.glob(os.path.join(output_dir, "controlnet_full_*.png"))
+    ):
+        with open(frame_path, "rb") as img_file:
+            response = requests.post(
+                docker_url,
+                headers={"Content-Type": "application/json"},
+                json={
+                    "input": {
+                        "image": f"data:image/png;base64,{base64.b64encode(img_file.read()).decode('utf-8')}",
+                    },
+                },
+            )
+        img = Image.open(
+            BytesIO(base64.b64decode(response.json()["output"][0].split(",")[1]))
+        )
+        img.save(os.path.join(output_dir, f"nobg_{i:05}.png"))
 
-    # Kill bgremoval docker container
+    # Kill controlnet docker container
+    docker_process.terminate()
     os.system("docker kill $(docker ps -q)")
 
     # Combine frames into video
-    os.system(f"ffmpeg -framerate 30 -i {output_dir}/raw_%05d.png -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p {output_dir}/output.mp4")
+    os.system(
+        f"ffmpeg -framerate 30 -i {output_dir}/nobg_%05d.png -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p {output_dir}/output.mp4"
+    )
+
+
+if __name__ == "__main__":
+    process_video_frames()
